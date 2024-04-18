dac9f2e68ea72ca36e37102abcb931199a7e4558
Dylandxb <dylanjmeadows@gmail.com>
2024-04-18 18:57:01 +0100

    Creation of base class
    - Test for sync

---
+++ GPC-Testing/PCManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPC_Testing
+{
+    class PCManager
+    {
+
+        public int id;
+        public string SKU;
+        public string CPU;
+        public string PSU;
+        public string MB;
+        public string RAM;
+        public string testerName;
+
+    }
+}
